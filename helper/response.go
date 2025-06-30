b66c7cd749d377662e4371d5d700300d50f8a6b2
dosedaf <mash1o1o1o1@gmail.com>
2025-06-30 15:28:50 +0700

    add: http helper

---
+++ helper/response.go
@@ -0,0 +1,56 @@
+package helper
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+	Data    string `json:"data"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
+
+func JSONResponse(w http.ResponseWriter, code int, data string) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	resp := &Response{
+		Message: "tes",
+		Data:    data,
+	}
+
+	b, err := json.Marshal(resp)
+	if err != nil {
+		return err
+	}
+
+	if _, err = w.Write(b); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func JSONError(w http.ResponseWriter, code int, msg string) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	resp := &Error{
+		Message: msg,
+	}
+
+	b, err := json.Marshal(resp)
+	if err != nil {
+		return err
+	}
+
+	if _, err = w.Write(b); err != nil {
+		return err
+	}
+
+	return nil
+}
